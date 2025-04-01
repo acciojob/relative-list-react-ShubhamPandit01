2cf658774968ba0735dcd959aeca62c5d8e1ddf4
Shubham Pandit <Shubhamproduction1@gmail.com>
2025-04-01 10:00:07 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const relatives = ['Amit', 'Priya', 'Suresh', 'Neha', 'Raj'];
   return (
     <div id="main">
                {/* Do not remove the main div */}
+          <ol key="relativeList">
+            {relatives.map((name, index)=>(
+              <li key={`relativeListItem${index + 1}`}>{name}</li>
+            ))}
+          </ol>
     </div>
   )
 }
